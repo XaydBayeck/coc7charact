e5510ca507259febb332a98819b3076ce77c35e0
Sid <a15348545942@outlook.com>
2019-10-20 14:45:57 +0800

    增加技能类
    增加技能类第一版以及其测试类和测试用的json文件

---
+++ src/main/kotlin/server/SkillData.kt
@@ -0,0 +1,17 @@
+package server
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class SkillData(
+        val name:String,
+        val kind:String,
+        val probability:Int,
+        val description:String,
+        val levelDescription:String,
+        val normalLevel:String,
+        val hardLevel:String,
+        val allEgg:String,
+        val allEggBreak: String,
+        val madEggBreak:String
+)
---
+++ src/test/kotlin/server/SkillTest.kt
@@ -0,0 +1,33 @@
+package server
+
+import kotlinx.serialization.ImplicitReflectionSerializer
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.list
+import kotlinx.serialization.parseList
+import org.junit.Assert.*
+import org.junit.Test
+import java.io.File
+
+class SkillTest {
+    @ImplicitReflectionSerializer
+    @Test
+    fun testSkill() {
+        val jsonPath = "src/resource/skill.json"
+        var jsonList = ""
+        File(jsonPath).forEachLine(action = {
+            jsonList += it
+            jsonList += "\n"
+        })
+        val skillsData = Json.parseList<SkillData>(jsonList)
+        val skills = ArrayList<Skill>(0)
+        for (skillData in skillsData) {
+            val skill = Skill(skillData)
+            skills.add(skill)
+        }
+
+        for (skill in skills) {
+            val str=skill.toString()
+            println(str)
+        }
+    }
+}
